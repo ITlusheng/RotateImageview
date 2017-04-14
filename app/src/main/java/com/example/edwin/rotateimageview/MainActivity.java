8e504f801949a7aaf93469d0c13f83be04095622
sfhan018 <sfhan018@163.com>
2017-04-14 16:24:27 +0800

    dhhhhhhhh

--- app/src/main/java/com/example/edwin/rotateimageview/MainActivity.java
+++ app/src/main/java/com/example/edwin/rotateimageview/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //第二次提交测试11100
+        //ffffffffffff
     }
 }
