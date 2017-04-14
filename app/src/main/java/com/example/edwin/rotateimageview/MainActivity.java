b1cd282b8956895d71b7501bb17367aa65b777a7
sfhan018 <sfhan018@163.com>
2017-04-14 16:29:33 +0800

    dhhhhhhhh

--- app/src/main/java/com/example/edwin/rotateimageview/MainActivity.java
+++ app/src/main/java/com/example/edwin/rotateimageview/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.edwin.rotateimageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //ffffffffffff
         //测试结果
+        initUI();
+    }
+
+    private void initUI() {
+
+        Toast.makeText(this,"123",Toast.LENGTH_SHORT).show();
     }
 }
